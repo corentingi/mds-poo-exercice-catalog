6bbf9c44d0bd9137d11daf987092d77bd7b884f8
Corentin Gitton <corentin.gitton@gmail.com>
2022-11-14 16:51:20 +0100

    feat(exercice 3): improve pagination

--- resources/views/paginator.blade.php
+++ resources/views/paginator.blade.php
@@ -1,10 +1,10 @@
 <ul>
-    @foreach ($elements[0] as $page => $link)
-    <li style="display:inline;">
-        @if ($page == $paginator->currentPage())
-        <span style="font-weight: bold;">{{ $page }}</span>
+    @foreach ($paginator->appends(request()->query())->linkCollection() as $link)
+    <li style="display:inline; margin: 0rem 0.2rem;">
+        @if ($link['active'])
+        <span style="font-weight: bold;">{!! $link['label'] !!}</span>
         @else
-        <a href="{{ $link }}">{{ $page }}</a>
+        <a href="{{ $link['url'] }}">{!! $link['label'] !!}</a>
         @endif
     </li>
     @endforeach
